d0fbd2b1c53cdcb54c16c961b3c5efd1d731113e
Benoît Eveillard <b.eveillard9@gmail.com>
2023-05-02 18:21:23 -0300

    modifs js

--- funnel.js
+++ funnel.js
@@ -220,7 +220,7 @@ function updateValue() {
     document.getElementById("don2_100").classList.add("hide");
     document.getElementById("don2_50").classList.add("hide");
     document.getElementById("don2_30").classList.add("hide");
-    document.getElementById("don_libre_wrapper").classList.add("hide");
+    document.getElementById("don_libre_component").classList.add("hide");
     document.getElementById("don_100").classList.remove("hide");
     document.getElementById("don_50").classList.remove("hide");
     document.getElementById("don_30").classList.remove("hide");
@@ -251,7 +251,7 @@ function updateValue() {
     document.getElementById("don2_100").classList.remove("hide");
     document.getElementById("don2_50").classList.remove("hide");
     document.getElementById("don2_30").classList.remove("hide");
-    document.getElementById("don_libre_wrapper").classList.remove("hide");
+    document.getElementById("don_libre_component").classList.remove("hide");
     document.getElementById("don_100").classList.add("hide");
     document.getElementById("don_50").classList.add("hide");
     document.getElementById("don_30").classList.add("hide");
@@ -320,7 +320,7 @@ function updateValue() {
     document.getElementById("don2_100").classList.remove("hide");
     document.getElementById("don2_50").classList.remove("hide");
     document.getElementById("don2_30").classList.remove("hide");
-    document.getElementById("don_libre_wrapper").classList.remove("hide");
+    document.getElementById("don_libre_component").classList.remove("hide");
     document.getElementById("don_100").classList.add("hide");
     document.getElementById("don_50").classList.add("hide");
     document.getElementById("don_30").classList.add("hide");
